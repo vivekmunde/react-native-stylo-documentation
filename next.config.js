e35db982d49bf7aa32ae842440afc724734a0f83
Vivek <vivekmunde@gmail.com>
2022-06-07 18:24:40 +0530

    basePath configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/react-native-stylo-documentation/',
+  assetPrefix: isProd ? '/react-native-stylo-documentation/' : '',
+  basePath: isProd ? '/react-native-stylo-documentation' : ''
 }
 
 module.exports = nextConfig

da11b2e2cc4ec05818e6f7e09d7f05921d516cc5
Vivek <vivekmunde@gmail.com>
2022-06-22 10:54:19 +0530

    logo & favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -34,8 +34,25 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               <ul className="mt-4 mb-8">
                 <li>
                   <Link href="/">
-                    <a className="font-bold">
-                      React Native Stylo
+                    <a
+                      style={{
+                        fontSize: '20px',
+                        fontWeight: '200',
+                        lineHeight: 1,
+                        whiteSpace: 'nowrap',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <span>
+                        react-native-
+                      </span>
+                      <img
+                        alt='stylo'
+                        src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/logo.png`}
+                        style={{ height: '28px' }}
+                      />
                     </a>
                   </Link>
                 </li>

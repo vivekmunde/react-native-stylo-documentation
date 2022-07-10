6759bfdc133db1776ee2d8b138be930002568354
Vivek <vivekmunde@gmail.com>
2022-07-10 12:14:16 +0530

    installation instructions

--- components/inline-code.tsx
+++ components/inline-code.tsx
@@ -4,7 +4,7 @@ const InlineCode: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => (
   <small>
-    <code className="text-black bg-gray-100 p-1 rounded" style={{ whiteSpace: 'nowrap' }}>
+    <code className="text-black bg-gray-100 p-1 rounded">
       {children}
     </code>
   </small>
--- pages/usage.tsx
+++ pages/usage.tsx
@@ -96,13 +96,7 @@ const Usage: React.FC = () => (
               Stylo uses <ExternalLink href="https://www.npmjs.com/package/react-native-vector-icons">Vector Icons</ExternalLink>, so it has a peer dependency of <InlineCode>react-native-vector-icons</InlineCode>@^9.1.0.
             </Paragraph>
             <Paragraph>
-              Stylo is in BETA currently &amp; available on GitHub for installation. Soon will be released on NPM.
-            </Paragraph>
-            <Paragraph>
-              <InlineCode>npm install --save react-native-vector-icons@^9.1.0</InlineCode>
-              <br />
-              <InlineCode>npm install --save react-native-stylo@git+https://github.com/vivekmunde/react-native-stylo.git</InlineCode>
-              {/* <InlineCode>npm install --save react-native-vector-icons@^9.1.0 react-native-stylo</InlineCode> */}
+              <InlineCode>npm install --save react-native-vector-icons@^9.1.0 react-native-stylo</InlineCode>
               <br />
               {' '}
               <br />
@@ -110,10 +104,7 @@ const Usage: React.FC = () => (
               <br />
               {' '}
               <br />
-              <InlineCode>yarn add react-native-vector-icons@^9.1.0</InlineCode>
-              <br />
-              <InlineCode>yarn add react-native-stylo@git+https://github.com/vivekmunde/react-native-stylo.git</InlineCode>
-              {/* <InlineCode>yarn add react-native-vector-icons@^9.1.0 react-native-stylo</InlineCode> */}
+              <InlineCode>yarn add react-native-vector-icons@^9.1.0 react-native-stylo</InlineCode>
             </Paragraph>
           </section>
           <section className="no-vertical-margin-collapse">
